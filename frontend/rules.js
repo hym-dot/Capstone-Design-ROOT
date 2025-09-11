3eabed7afda56754adf86619db7d8907f8445230
hym-dot <yumin6350@kbu.ac.kr>
2025-09-11 17:32:19 +0900

    .

--- frontend/rules.js
+++ frontend/rules.js
@@ -1,4 +1,4 @@
 // rules.js
-export const blacklist = ["phishingsite.com", "malicious-login.net"];
+export const blacklist = ["test-phishing.com"];
 export const keywords = ["login-verification", "secure-update", "banking"];
 export const patterns = [/free-\w+-gift/i, /verify-\w+-account/i];
